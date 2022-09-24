7cf2a7d5ce10b286950c31507858470979cae20f
Ruan Aragão <ruanaragao2@gmail.com>
2022-09-24 17:47:18 -0300

    refactor directory structures

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Ruan Aragão <ruanaragao2@gmail.com>'
-description 'Simple Notepad'
+description 'MSK Simple Notepad'
 version '1.0.0'
 
 server_scripts {
@@ -15,5 +15,7 @@ client_script 'client.lua'
 ui_page 'nui/main.html'
 files {
   'nui/*.html',
-  'nui/images/*.svg'
+  'nui/images/*.svg',
+  'nui/js/*.js',
+  'nui/css/*.css'
 }
